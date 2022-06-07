fc78e492adb437b45151f62cf848a4521b2a7fd0
Vadim <vadik.kortkov@yandex.ru>
2022-06-07 15:54:57 +0300

    form add

--- src/templates/template1/template1.js
+++ src/templates/template1/template1.js
@@ -1,13 +1,16 @@
 import './template1.scss'
 import SectionHeader from "../../organisms/sectionHeader/sectionHeader";
 import SectionMain from "../../organisms/sectionMain/sectionMain";
-
+import SectionForm from "../../organisms/sectionForm/sectionForm"
+import SectionFooter from "../../organisms/sectionFooter/sectionFooter"
 
 export default function Template1() {
     return(
         <div className="template1">
             <SectionHeader />
             <SectionMain />
+            <SectionForm/>
+            <SectionFooter/>
         </div>
     )
 }
