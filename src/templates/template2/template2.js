0b9e4435cba6bc79bdf645ea41bde639399ba129
BulatGim <bulat.gimaletdinov.01@gmail.com>
2022-06-08 12:40:59 +0300

    form in second template added

--- src/templates/template2/template2.js
+++ src/templates/template2/template2.js
@@ -2,13 +2,15 @@ import SectionHeader from './organisms/sectionHeader/sectionHeader';
 import SectionInfo from './organisms/sectionInfo/sectionInfo';
 import SectionFooter from "./organisms/sectionFooter/sectionFooter";
 import './template2.scss';
+import SectionForm from "./organisms/sectionForm/sectionForm";
 
 export default function Template2(){
     return (
         <div className="template2">
-            <SectionHeader/>
+            {/*<SectionHeader/>*/}
             <SectionInfo/>
             {/*<div style={{width:'100%', height:'100vh',backgroundColor:'red'}}></div>*/}
+            <SectionForm />
             <SectionFooter />
         </div>
     )
