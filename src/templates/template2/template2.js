5565c22038ec7ae206df3de61921507459fd58b8
BulatGim <bulat.gimaletdinov.01@gmail.com>
2022-06-08 16:58:54 +0300

    reviews added and footer media changed

--- src/templates/template2/template2.js
+++ src/templates/template2/template2.js
@@ -3,6 +3,7 @@ import SectionAdvantages from './organisms/sectionAdvantages/sectionAdvantages';
 import SectionHeader from './organisms/sectionHeader/sectionHeader';
 import SectionInfo from './organisms/sectionInfo/sectionInfo';
 import SectionFooter from "./organisms/sectionFooter/sectionFooter";
+import SectionReviews from "./organisms/sectionReviews/sectionReviews";
 import './template2.scss';
 import SectionForm from "./organisms/sectionForm/sectionForm";
 
@@ -13,6 +14,7 @@ export default function Template2(){
             <SectionInfo/>
             <SectionAboutCompany/>
             <SectionAdvantages/>
+            <SectionReviews />
             <SectionForm />
             <SectionFooter />
         </div>
