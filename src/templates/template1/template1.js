814e319561505e1a712dd4bdba6744fc4a0b9a4d
Vadim <vadik.kortkov@yandex.ru>
2022-06-08 09:19:12 +0300

    reviews

--- src/templates/template1/template1.js
+++ src/templates/template1/template1.js
@@ -3,6 +3,7 @@ import SectionHeader from "../../organisms/sectionHeader/sectionHeader";
 import SectionMain from "../../organisms/sectionMain/sectionMain";
 import SectionAboutUs from "../../organisms/sectionAboutUs/sectionAboutUs";
 import SectionServices from "../../organisms/sectionServices/sectionServices";
+import SectionReviews from "../../organisms/sectionReviews/sectionReviews";
 import SectionForm from "../../organisms/sectionForm/sectionForm"
 import SectionFooter from "../../organisms/sectionFooter/sectionFooter"
 
@@ -13,6 +14,7 @@ export default function Template1() {
             <SectionMain />
             <SectionAboutUs />
             <SectionServices />
+            <SectionReviews/>
             <SectionForm/>
             <SectionFooter/>
         </div>
