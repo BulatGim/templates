0e466d1e4754a9e05a41000d3d5dfe22035d47b7
Vadim <vadik.kortkov@yandex.ru>
2022-06-10 15:40:40 +0300

    template3 about

--- src/templates/template3/organisms/sectionAbout/sectionAbout.js
+++ src/templates/template3/organisms/sectionAbout/sectionAbout.js
@@ -1,8 +1,16 @@
 import './sectionAbout.scss';
-export default function SectionAbout (){
+import about from './imgs/about.svg'
+export default function SectionAbout() {
     return (
         <div className="template3SectionAbout">
-            
+            <div className="template3SectionAbout__container">
+                <div className="template3SectionAbout__info">
+                    <h1 className="template3SectionAbout__title">Заголовок сайта</h1>
+                    <h4 className="template3SectionAbout__subtitle">Добавьте интересные подробности о вашей компании.</h4>
+                    <button className="template3SectionAbout__button"><h4>Призыв к действию</h4></button>
+                </div>
+                <img src={about} alr="Сайт" className="template3SectionAbout__img"/>
+            </div>
         </div>
     )
 }
--- src/templates/template3/template3.js
+++ src/templates/template3/template3.js
@@ -1,8 +1,9 @@
+import SectionAbout from './organisms/sectionAbout/sectionAbout';
 import './template3.scss';
 export default function Template3(){
     return (
         <div className="template3">
-            
+            <SectionAbout/>
         </div>
     )
 }
