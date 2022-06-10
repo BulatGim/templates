a1b22b41094404a26e6290acad86aecf173b367f
Vadim <vadik.kortkov@yandex.ru>
2022-06-10 15:08:48 +0300

    template3

---
+++ src/templates/template3/organisms/sectionAbout/sectionAbout.js
@@ -0,0 +1,8 @@
+import './sectionAbout.scss';
+export default function SectionAbout (){
+    return (
+        <div className="template3SectionAbout">
+            
+        </div>
+    )
+}
---
+++ src/templates/template3/template3.js
@@ -0,0 +1,8 @@
+import './template3.scss';
+export default function Template3(){
+    return (
+        <div className="template3">
+            
+        </div>
+    )
+}
