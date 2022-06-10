3555c7d3d62c654bfb456111dfefb09ceffe42bc
Vadim <vadik.kortkov@yandex.ru>
2022-06-10 16:18:49 +0300

    template3 header

---
+++ src/templates/template3/organisms/sectionHeader/links.js
@@ -0,0 +1,8 @@
+export const links = [
+    { id: 1, to: '', text: 'Товар' },
+    { id: 2, to: '', text: 'Преимущества' },
+    { id: 3, to: '', text: 'Гарантии' },
+    { id: 4, to: '', text: 'Отзывы' },
+    { id: 5, to: '', text: 'FAQ' },
+    { id: 6, to: '', text: 'Контакты' },
+];
--- src/templates/template3/template3.js
+++ src/templates/template3/template3.js
@@ -1,8 +1,10 @@
 import SectionAbout from './organisms/sectionAbout/sectionAbout';
+import SectionHeader from './organisms/sectionHeader/sectionHeader';
 import './template3.scss';
 export default function Template3(){
     return (
         <div className="template3">
+            <SectionHeader/>
             <SectionAbout/>
         </div>
     )
