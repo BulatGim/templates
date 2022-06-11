aded644a31a91040189d39fa6db2f4d35cfa5572
Vadim <vadik.kortkov@yandex.ru>
2022-06-11 10:24:26 +0300

    template3 product

--- src/templates/template3/template3.js
+++ src/templates/template3/template3.js
@@ -1,11 +1,13 @@
 import SectionAbout from './organisms/sectionAbout/sectionAbout';
 import SectionHeader from './organisms/sectionHeader/sectionHeader';
+import SectionProduct from './organisms/sectionProduct/sectionProduct';
 import './template3.scss';
 export default function Template3(){
     return (
         <div className="template3">
             <SectionHeader/>
             <SectionAbout/>
+            <SectionProduct/>
         </div>
     )
 }
